af11a2a8c7a268c51f3244b24099578e8fcd9ebd
hansenjl <jenn.leigh.hansen@gmail.com>
2020-05-25 11:35:17 -0700

    first commit

---
+++ lib/drinkmaster.rb
@@ -0,0 +1,6 @@
+require "drinkmaster/version"
+
+module Drinkmaster
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/drinkmaster/version.rb
@@ -0,0 +1,3 @@
+module Drinkmaster
+  VERSION = "0.1.0"
+end
